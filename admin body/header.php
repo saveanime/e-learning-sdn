f8f5e57c9e686090105a158e4baf6c0c385e2181
saveanime <83510535+saveanime@users.noreply.github.com>
2021-05-09 02:20:30 +0700

    Add files via upload

---
+++ admin body/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Collapsible sidebar using Bootstrap 4</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="../admin css/style.css">
+    <link rel="stylesheet" href="../admin css/admin.css">
+
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>SDN terserah</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <li>
+                    <a href="dashboard.php">Home</a>
+                </li>
+                <li>
+                    <a href="soal.php">Soal</a>
+                </li>
+                <li>
+                    <a href="pembelajaran.php">Pembelajaran</a>
+                </li>
+                <li>
+                    <a href="nilai.php">Nilai</a>
+                </li>
+                <li>
+                    <a href="absen.php">Absen</a>
+                </li>
+            </ul>
+
+            <ul class="list-unstyled CTAs">
+                <li>
+                    <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
+                </li>
+                <li>
+                    <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
+                </li>
+            </ul>
+        </nav>
---
+++ admin body/pembelajaran.php
@@ -0,0 +1,86 @@
+<?php include "header.php";?>
+<!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span> geser ke kiri </span>
+                    </button>
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <!-- d soal -->
+            <div class="row">
+            <div class="row text-white">
+            <div class="card bg-info ml-5" style="width: 17rem; height: 12rem;">
+              <div class="card-body">
+                <div class="card-body-icon">
+                    <i class="fas fa-user-graduate mr-2"></i>
+                </div>
+                <h5 class="card-title">SOAL</h5><br><br><br><br>
+                <a href=""><p class="card-text text-white">Lihat detail <i class="fas fa-angle-double-right ml-2"></i></p></a>
+              </div>
+            </div>
+            </div>
+
+            <!-- d pembelajaran -->
+            
+            <div class="row text-white">
+            <div class="card bg-info ml-5" style="width: 17rem; height: 12rem;">
+              <div class="card-body">
+                <div class="card-body-icon">
+                    <i class="fas fa-user-graduate mr-2"></i>
+                </div>
+                <h5 class="card-title">PEMBELAJARAN</h5><br><br><br><br>
+                <a href=""><p class="card-text text-white">Lihat detail <i class="fas fa-angle-double-right ml-2"></i></p></a>
+              </div>
+            </div>
+            </div>
+            </div><br>
+
+            <!-- d nilai -->
+            <div class="row">
+            <div class="row text-white">
+            <div class="card bg-info ml-5" style="width: 17rem; height: 12rem;">
+              <div class="card-body">
+                <div class="card-body-icon">
+                    <i class="fas fa-user-graduate mr-2"></i>
+                </div>
+                <h5 class="card-title">NILAI</h5><br><br><br><br>
+                <a href=""><p class="card-text text-white">Lihat detail <i class="fas fa-angle-double-right ml-2"></i></p></a>
+              </div>
+            </div>
+            </div>
+            
+
+             <!-- d absen -->
+            <div class="row text-white">
+            <div class="card bg-info ml-5" style="width: 17rem; height: 12rem;">
+              <div class="card-body">
+                <div class="card-body-icon">
+                    <i class="fas fa-user-graduate mr-2"></i>
+                </div>
+                <h5 class="card-title">ABSEN</h5><br><br><br><br>
+                <a href=""><p class="card-text text-white">Lihat detail <i class="fas fa-angle-double-right ml-2"></i></p></a>
+              </div>
+            </div>
+            </div>
+        </div>
+
+            
+
+        </div>
+    </div>
+<?php include "footer.php";?>
